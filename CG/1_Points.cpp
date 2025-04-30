aaf5b519d7ec6211a7ca1b6aa3ef30d71c835c1c
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-30 18:20:27 +0530

    Update 1_Points.cpp

--- CG/1_Points.cpp
+++ CG/1_Points.cpp
@@ -13,9 +13,9 @@ void display() {
 
 void myinit() {
   glClearColor(1.0,1.0,1.0,1.0);
-  glColor3f(1.0,1.0,1.0);
+  glColor3f(1.0,0.0,0.0);
   glPointSize(20.0);
-    gluOrtho2D(0.0,499.0,0.0,499.0);
+  gluOrtho2D(0.0,499.0,0.0,499.0);
 }
 
 int main(int argc, char** argv) {
