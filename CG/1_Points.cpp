4fe783f2028f9f32fe29210a7af5873e0e2d7d51
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-30 18:43:01 +0530

    Update 1_Points.cpp

--- CG/1_Points.cpp
+++ CG/1_Points.cpp
@@ -12,8 +12,8 @@ void display() {
 }
 
 void myinit() {
-  glClearColor(1.0,0.0,0.0);
-  glColor3f(1.0,0.0,0.0);
+  glClearColor(1.0,1.0,1.0,1.0);
+  glColor3f(0.0,0.0,1.0);
   glPointSize(20.0);
   gluOrtho2D(0.0,499.0,0.0,499.0);
 }
@@ -22,7 +22,7 @@ void myinit() {
 int main(int argc , char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  gkutInitWindowSize(1080,720);
+  glutInitWindowSize(1080,720);
   glutInitWindowPosition(0.0,0.0);
   glutCreateWindow("Points Program");
   
