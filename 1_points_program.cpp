82ccf490c5072713efb5bd3946f9d60947a39685
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-30 17:32:45 +0530

    Update 1_points_program.cpp

--- 1_points_program.cpp
+++ 1_points_program.cpp
@@ -1,10 +1,19 @@
 #include<GL/glut.h>
 
+
+void display() {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glColor3f(1.0,0.0,0.0);
+    glBegin(GL_POINTS);
+        glVertex2f(10.0,10.0);
+    glEnd();
+    glFlush();
+}
+
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(500,500);
     glutInitWindowPosition(0,0);
     glutCreateWindow("Points");
-    
 }
