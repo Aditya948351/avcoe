b6541c220817fb2cef742d1003ed2c22570a98bf
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-30 18:41:25 +0530

    Update 1_Points.cpp

--- CG/1_Points.cpp
+++ CG/1_Points.cpp
@@ -1,32 +1,34 @@
 #include<GL/glut.h>
 
-
 void display() {
-  glClear(GL_COLOR_BUFFER_BIT); //Clears the buffer
-  glColor3f(1.0,0.0,0.0); //every vertex has its own color
+  glClear(GL_COLOR_BUFFER_BIT);
+  glColor3f(1.0,1.0,1.0,1.0);
   glBegin(GL_POINTS);
-    glVertex2f(10.0,10.0);
-    glVertex2f(150,180);
+  glVertex2f(10.0,10.0);
+  
   glEnd();
   glFlush();
+
 }
 
 void myinit() {
-  glClearColor(1.0,1.0,1.0,1.0);
+  glClearColor(1.0,0.0,0.0);
   glColor3f(1.0,0.0,0.0);
   glPointSize(20.0);
   gluOrtho2D(0.0,499.0,0.0,499.0);
 }
 
-int main(int argc, char** argv) {
+
+int main(int argc , char** argv) {
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-  glutInitWindowSize(1080,720);
-  glutInitWindowPosition(0,0);
-  glutCreateWindow("1. Points");
-
-  glutDisplayFunc(display);  
+  gkutInitWindowSize(1080,720);
+  glutInitWindowPosition(0.0,0.0);
+  glutCreateWindow("Points Program");
+  
+  glutDisplayFunc(display);
   myinit();
   
   glutMainLoop();
+  
 }
