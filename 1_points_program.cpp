929b143f68f4b8b8e4de6c287b26a08fda79e5ff
Aditya Amol Patil <157286761+Aditya948351@users.noreply.github.com>
2025-04-30 17:46:49 +0530

    Update 1_points_program.cpp

--- 1_points_program.cpp
+++ 1_points_program.cpp
@@ -13,7 +13,7 @@ void display() {
 void myinit() {
     glClearColor(0.0,0.0,0.0,0.0);
     glColor3f(1.0,0.0,0.0);
-    glPointSize(10.0);
+    glPointSize(20.0);
     gluOrtho2D(0.0,499.0,0.0,0.499);
 }
 
